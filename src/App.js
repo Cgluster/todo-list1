37ba4d7fe7d5a0d0da2a7ada1f5d2a266fbf19d8
unknown <luster.clifford@yahoo.com>
2022-09-27 19:25:04 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To Do List with React</h1>
       </header>
+      <body>
+        <h2>Please Add a Task</h2>
+        <input type="text" />
+        <button type='submit'>Submit</button>
+        <hr />
+        <h2>Tasks To Accomplish</h2>
+        <p>Take Shower</p>
+        <p>Exercise - 1 hour</p>
+        <p>Code - 4 hours</p>
+      </body>
     </div>
   );
 }
